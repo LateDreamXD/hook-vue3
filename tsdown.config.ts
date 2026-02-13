eb135d4db8b26585ecaf9447b9e591d5d32d47ed
LateDreamXD <127290936+LateDreamXD@users.noreply.github.com>
2026-02-13 14:04:50 +0800

    chore(logger): allow always debug log out

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-	define: { 'process.env.NODE_ENV': JSON.stringify('production') },
 	entry: { content: 'src/content/index.ts' },
 	minify: true,
 	dts: false,
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import type { UserConfig } from 'vite';
 import vue from 'unplugin-vue/vite';
 
 export default <UserConfig>{
-	define: {
-		'process.env.NODE_ENV': JSON.stringify('development'),
-	},
 	plugins: [vue()],
 };
